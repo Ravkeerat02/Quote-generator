08e33e44622e0af476f4b1908d82f319c5239f55
Ravkeerat Singh <ravkeerat.singh02@gmail.com>
2023-04-04 03:06:58 -0400

    small fix

--- script.js
+++ script.js
@@ -90,7 +90,8 @@ async function getNewQuote() {
       const minutesDegrees = ((minutes / 60) * 360) + 90;
       minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
-      const hours = now.getHours();
+      let hours = now.getHours();
+      hours = hours > 12 ? hours - 12 : hours;
       const hoursDegrees = ((hours / 12) * 360) + 90;
       hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
   }
